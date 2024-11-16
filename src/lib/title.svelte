f4640f1475fc4b9cecc2be9fcd63fc3079dac1b2
sajjad314 <hossainsazzad314@gmail.com>
2024-11-16 20:43:34 +0600

    project initialization with tailwind and implement navbar

---
+++ src/lib/title.svelte
@@ -0,0 +1,9 @@
+<script>
+  let title = "My app";
+</script>
+
+<div class="w-full border-b-2 border-gray-200 mb-12">
+  <div class="container mx-auto flex justify-center py-4">
+    <img src="/logo.png" alt="site logo" width="300px" />
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Title from '$lib/title.svelte'
+    import "./index.css"
+    
+</script>
+<Title/>
+<div class="flex gap-x-10 mx-24">
+    
+
+</div>
